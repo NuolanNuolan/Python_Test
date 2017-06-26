ae46272b2dab6e929effb307e28b846b688b0c26
NuolanNuolan <yllwangyi@163.com>
2017-06-26 15:10:42 +0800

    更新

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 from def_hello import *
 from collections import OrderedDict
-
 
 # //大小写
 # name = "ada lovalcase"
@@ -69,7 +69,7 @@ from collections import OrderedDict
 # squares = []
 # for value in range(1,101):
 #   squares.append(value)
-
+#
 # print(squares)
 # print(len(squares))
 # for value1 in squares[:3]:
@@ -143,7 +143,7 @@ from collections import OrderedDict
 # name = make_pizza('size','name')
 # print(name)
 # my_dog = electri("hehehe",17)
-your_dog = Dog("hahaha", '20')
+# your_dog = Dog("hahaha", '20')
 # my_dog.sit()
 # your_dog.sit()
 # print(my_dog.age)
@@ -152,10 +152,88 @@ your_dog = Dog("hahaha", '20')
 # my_dog.battery_size.describe_battery()
 
 
-favorite_languages = OrderedDict()
+# favorite_languages = OrderedDict()
+#
+# favorite_languages['jen'] = '中文'
+# favorite_languages['yll'] = 'oc'
+# favorite_languages['phil'] = 'ruby'
+# for [key, value] in favorite_languages.items():
+#     print(key, value)
+# print('我是中文')
 
-favorite_languages['jen'] = 'python'
-favorite_languages['yll'] = 'oc'
-favorite_languages['phil'] = 'ruby'
-for key, value in favorite_languages.items():
-    print(key, value)
+#%5==0
+# def Orial(num):
+#     while num%5 == 0:
+#         print("%5等于0")
+#         break
+# def Num(n):
+#     for num in range(1,n+1):
+#         Orial(num)
+#         print(num)
+# 定义阶乘
+# def Num(n):
+#     if n < 2:
+#         return 1
+#     else:
+#         return n * Num(n-1)
+# num = input("数字:")
+# print(Num(int(num)))
+
+#读取文件
+# with open('pi_digits.txt') as file_object:
+#     contents = file_object.read()
+#     print(contents.rstrip())
+
+# filename = 'pi_digits.txt'
+
+# with open(filename) as  file_object:
+#     for line in  file_object:
+#         print(line.rstrip())
+#
+# with open(filename) as  file_object:
+#     lines =  file_object.readlines()
+#     print(lines)
+# pi_string = ''
+# for line in lines:
+#     pi_string += line.strip()
+# print(pi_string)
+# if '27137' in pi_string:
+#    pi_string =  pi_string.replace('27137','123456')
+# else :
+#     print("No")
+# print(pi_string)
+
+# filename = 'write_messaage.txt'
+#
+# with open(filename,'a') as file_object:
+#     file_object.write('hello world')
+#
+# with open(filename) as  file_object:
+#     print(file_object.read())
+
+# name = input("请输入名字:")
+# filename  = 'name.txt'
+# with open(filename , 'w') as file_object:
+#     file_object.write(name)
+#
+# with open(filename) as file_object:
+#     print(file_object.read())
+
+#异常
+# print(5/0)
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print('msg:ZeroDivisionError错误')
+#
+# try:
+#     filename = 'file.txt'
+#     with open(filename,'r') as file_object:
+#         print(file_object.read())
+# except FileNotFoundError:
+#     print('msg:文件不存在')
+#分割字符串 指定分割字符 以及次数
+# title = "Ailce in wonderland"
+# title = title.split(' ')
+# print(title)
+
